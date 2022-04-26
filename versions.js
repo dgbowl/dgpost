3f7fc1b931905a2bcc280354e62295a58087834c
krpe <peter.kraus@empa.ch>
2022-04-26 08:07:14 +0200

    Version 1.0 of docs.

---
+++ versions.js
@@ -0,0 +1,9 @@
+arr = window.location.href.split('/');
+page = arr[arr.length - 1];
+document.write('\
+<dl>\
+    <dt>Versions</dt> \
+    <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../1.0/' + page + '">1.0</a></dd>\
+</dl>\
+');
