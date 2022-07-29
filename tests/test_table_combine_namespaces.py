6fbaa261e1f29b2e2936d1117cf7b8b946f6e0de
krpe <peter.kraus@empa.ch>
2022-07-29 13:11:41 +0200

    Move namespace->table, add columns.

--- tests/test_table_combine_namespaces.py
+++ tests/test_table_combine_namespaces.py
@@ -3,7 +3,7 @@ import pytest
 import pandas as pd
 import numpy as np
 
-from dgpost.transform import namespace
+from dgpost.transform import table
 from dgpost.utils import transform
 from .utils import compare_dfs
 
@@ -45,10 +45,26 @@ from yadg.dgutils import ureg
                 "c->c": ureg.Quantity(20.0, "mmol/s"),
             },
         ),
+        (  # ts2 - sum with nan
+            {
+                "a": ureg.Quantity(10.0, "ml/min"),
+                "b": ureg.Quantity(float("NaN"), "ml/min"),
+            },
+            {
+                "b": ureg.Quantity(0.6, "l/h"),
+                "c": ureg.Quantity(6.0, "l/h"),
+            },
+            "sum",
+            {
+                "c->a": ureg.Quantity(10.0, "ml/min"),
+                "c->b": ureg.Quantity(10.0, "ml/min"),
+                "c->c": ureg.Quantity(100.0, "ml/min"),
+            },
+        ),
     ],
 )
-def test_namespace_combine_direct(a, b, conflicts, output):
-    ret = namespace.combine(a=a, b=b, conflicts=conflicts, output="c")
+def test_table_combine_namespace_direct(a, b, conflicts, output):
+    ret = table.combine_namespaces(a=a, b=b, conflicts=conflicts, output="c")
     for k, v in ret.items():
         assert np.allclose(v, output[k])
 
@@ -61,15 +77,20 @@ def test_namespace_combine_direct(a, b, conflicts, output):
             [{"a": "rin(GC)", "b": "rin(LC)", "output": "rtot"}],
             "rtot",
         ),
-        (  # ts0 - sum
+        (  # ts1 - sum
             "namespaces_2.xlsx",
+            [{"a": "rin(GC)", "b": "rin(LC)", "output": "rtot", "chemicals": True}],
+            "rtot",
+        ),
+        (  # ts2 - sum with nans
+            "namespaces_3.xlsx",
             [{"a": "rin(GC)", "b": "rin(LC)", "output": "rtot", "chemicals": True}],
             "rtot",
         ),
     ],
 )
-def test_namespace_combine_excel(inpath, spec, col, datadir):
+def test_table_combine_namespace_excel(inpath, spec, col, datadir):
     os.chdir(datadir)
     df = pd.read_excel(inpath)
-    df = transform(df, "namespace.combine", using=spec)
+    df = transform(df, "table.combine_namespaces", using=spec)
     compare_dfs(df[f"r{col}"], df[col])
