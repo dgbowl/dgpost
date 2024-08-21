281b9fd8b5f746c3ddeaccbaf6f0947b6da283e0
Peter Kraus <peter.kraus@tu-berlin.de>
2024-08-21 10:09:04 +0200

    Fix versioning.

--- src/dgpost/__init__.py
+++ src/dgpost/__init__.py
@@ -1,8 +1,11 @@
 import sys
+from importlib.metadata import version
+from .main import run_with_arguments
+
+__all__ = ["run_with_arguments"]
+__version__ = version("dgpost")
 
 sys.path += sys.modules["dgpost"].__path__
 
-from .main import run_with_arguments, run
-from . import _version
 
-__version__ = _version.get_versions()["version"]
+
