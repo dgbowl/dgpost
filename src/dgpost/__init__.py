4ec4f17b30ad93d656eb7d87afe10386e58aeae6
Peter Kraus <peter.kraus@tu-berlin.de>
2024-08-23 13:49:25 +0200

    fix uncertainties to < 3.2 for now. Re-add run.

--- src/dgpost/__init__.py
+++ src/dgpost/__init__.py
@@ -1,8 +1,8 @@
 import sys
 from importlib.metadata import version
-from .main import run_with_arguments
+from .main import run_with_arguments, run
 
-__all__ = ["run_with_arguments"]
+__all__ = ["run_with_arguments", "run"]
 __version__ = version("dgpost")
 
 sys.path += sys.modules["dgpost"].__path__
