4c6dabcd4b6ef5338f0b4e072c10d90a29665b9b
Peter Kraus <peter.kraus@tu-berlin.de>
2025-06-29 11:06:44 +0200

    tests

--- tests/test_mixtures_flowtofraction.py
+++ tests/test_mixtures_flowtofraction.py
@@ -2,13 +2,13 @@ import os
 import numpy as np
 import pandas as pd
 
-from dgpost.transform import rates
+from dgpost.transform import mixtures
 
 
 def test_rates_flowtofraction_pkl(datadir):
     os.chdir(datadir)
     df = pd.read_pickle("ftot.pkl")
-    df = rates.flow_to_fraction(df, total="ftot", vdot="f", output="x")
+    df = mixtures.flows_to_fractions(df, total="ftot", vdot="f", output="x")
     print(f"{df=}")
     assert np.allclose(df[("x", "a")], df[("xref", "a")])
     assert np.allclose(df[("x", "b")], df[("xref", "b")])
@@ -17,7 +17,7 @@ def test_rates_flowtofraction_pkl(datadir):
 def test_rates_flowtofraction_nan(datadir):
     os.chdir(datadir)
     df = pd.read_pickle("ftot.nan.pkl")
-    df = rates.flow_to_fraction(df, total="ftot", vdot="f", output="x")
+    df = mixtures.flows_to_fractions(df, total="ftot", vdot="f", output="x")
     print(f"{df=}")
     assert np.allclose(df[("x", "a")], df[("xref", "a")], equal_nan=True)
     assert np.allclose(df[("x", "b")], df[("xref", "b")], equal_nan=True)
