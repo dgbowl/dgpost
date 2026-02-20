`transform.impedance`: Fix units of Warburg and Constant Phase elements (#120)
* Fix unit conversion issues in impedance

* Fix more typos.

* Add test.

* test fix.

* Pin pandas.

src/dgpost/transform/circuit_utils/circuit_components.py
@@ -137,12 +137,13 @@ class CPE(Component):
 
     @staticmethod
     def get_parameters(name):
-        param1 = create_parameter(name + "_Q", (1e-20, 1), "Ω^-1 s^n")
+        param1 = create_parameter(name + "_Q", (1e-20, 1), "Ω^-1 s^a")
         param2 = create_parameter(name + "_a", (0, 1), "")
         return [param1, param2]
 
     @staticmethod
     def calc_impedance(param, name, freq):
+        # Zcpe = 1 / Q(jωᵃ) = jω⁻ᵃ / Q
         q = param.get(name + "_Q")
         n = param.get(name + "_a")
         result = (1j * 2 * np.pi * freq) ** (-n) / q
@@ -158,11 +159,12 @@ class Warburg(Component):
 
     @staticmethod
     def get_parameters(name):
-        param = create_parameter(name, (0, 1e10), "Ω^-1 s^-1/2")
+        param = create_parameter(name, (0, 1e10), "Ω s^(-1/2)")
         return [param]
 
     @staticmethod
     def calc_impedance(param, name, freq):
+        # Zw = [σω⁻¹ᐟ² - j(σω⁻¹ᐟ²)] = σ(1-j) / ω¹ᐟ²
         value = param.get(name)
         result = value * (1 - 1j) / np.sqrt(2 * np.pi * freq)
         return np.array(result)